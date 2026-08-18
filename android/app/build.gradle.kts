ec2093abf76f3684280c3bc0005bfaa7d9623ad3
Leonard James Melton <leonardmelton037@gmail.com>
2026-08-17 20:10:26 -0500

    fix(android): use KSP for Room code generation

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -2,7 +2,7 @@ plugins {
     id("com.android.application")
     id("org.jetbrains.kotlin.android")
     id("org.jetbrains.kotlin.plugin.compose")
-    id("androidx.room")
+    id("com.google.devtools.ksp")
 }
 
 android {
