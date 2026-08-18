7dbe2089ab137d7fde7e3ec37015cf4f200b0c2c
Leonard James Melton <leonardmelton037@gmail.com>
2026-08-17 20:09:47 -0500

    feat(android): pin current build toolchain

---
+++ android/build.gradle.kts
@@ -0,0 +1,6 @@
+plugins {
+    id("com.android.application") version "9.3.1" apply false
+    id("org.jetbrains.kotlin.android") version "2.3.21" apply false
+    id("org.jetbrains.kotlin.plugin.compose") version "2.3.21" apply false
+    id("androidx.room") version "2.8.4" apply false
+}
