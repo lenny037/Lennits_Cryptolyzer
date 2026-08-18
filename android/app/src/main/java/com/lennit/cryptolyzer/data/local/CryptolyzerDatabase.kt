48f3a5dbd8e8472b8e1953cea676dbc964df69a2
Leonard James Melton <leonardmelton037@gmail.com>
2026-08-17 20:23:02 -0500

    feat(data): persist Cortex memory in Room

--- android/app/src/main/java/com/lennit/cryptolyzer/data/local/CryptolyzerDatabase.kt
+++ android/app/src/main/java/com/lennit/cryptolyzer/data/local/CryptolyzerDatabase.kt
@@ -2,8 +2,15 @@ package com.lennit.cryptolyzer.data.local
 
 import androidx.room.Database
 import androidx.room.RoomDatabase
+import com.lennit.cryptolyzer.core.memory.AgentMemoryDao
+import com.lennit.cryptolyzer.core.memory.AgentMemoryEntity
 
-@Database(entities = [DomainEventEntity::class], version = 1, exportSchema = true)
+@Database(
+    entities = [DomainEventEntity::class, AgentMemoryEntity::class],
+    version = 2,
+    exportSchema = true
+)
 abstract class CryptolyzerDatabase : RoomDatabase() {
     abstract fun domainEventDao(): DomainEventDao
+    abstract fun agentMemoryDao(): AgentMemoryDao
 }
