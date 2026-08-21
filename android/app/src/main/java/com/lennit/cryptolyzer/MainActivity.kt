15d474dc2e07d58dc1626d70605ea462d1ee7043
Leonard James Melton <leonardmelton037@gmail.com>
2026-08-21 16:47:17 -0500

    runtime: decouple activity destruction from process runtime

--- android/app/src/main/java/com/lennit/cryptolyzer/MainActivity.kt
+++ android/app/src/main/java/com/lennit/cryptolyzer/MainActivity.kt
@@ -2,20 +2,9 @@ package com.lennit.cryptolyzer
 
 import android.app.Activity
 import android.os.Bundle
-import com.lennit.cryptolyzer.runtime.RuntimeLifecycle
 
 class MainActivity : Activity() {
-    private val runtimeLifecycle = RuntimeLifecycle()
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        runtimeLifecycle.start()
-    }
-
-    override fun onDestroy() {
-        if (runtimeLifecycle.state != RuntimeLifecycle.State.NEW) {
-            runtimeLifecycle.stop()
-        }
-        super.onDestroy()
     }
 }
